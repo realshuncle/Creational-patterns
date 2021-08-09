84a7796767b8c71b2bc31212e1e9113a9d230905
realshuncle <realshuncle@gmail.com>
2021-08-09 11:52:23 +0400

    factory method

---
+++ Creational patterns/Factory Method.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creational_patterns
+{
+    abstract class Product
+    { }
+
+    class ConcreteProductA : Product
+    { }
+
+    class ConcreteProductB : Product
+    { }
+
+    abstract class Creator
+    {
+        public abstract Product FactoryMethod();
+    }
+
+    class ConcreteCreatorA : Creator
+    {
+        public override Product FactoryMethod() { return new ConcreteProductA(); }
+    }
+
+    class ConcreteCreatorB : Creator
+    {
+        public override Product FactoryMethod() { return new ConcreteProductB(); }
+    }
+}
